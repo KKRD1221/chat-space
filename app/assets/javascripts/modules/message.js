bf9b373859d218287505c5dd9e7951dc7c86bba6
KKRD1221 <62683418+KKRD1221@users.noreply.github.com>
2020-07-22 13:49:31 +0900

    自動更新機能実装

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="main__messagebox">
+        `<div class="main__messagebox" data-message-id=${message.id}>
           <div class="main__messageinfo">
             <div class="main__username">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="main__messagebox">
+      `<div class="main__messagebox" data-message-id=${message.id}>
         <div class="main__messageinfo">
           <div class="main__username">
             ${message.user_name}
@@ -61,6 +61,7 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.main__send').prop("disabled", false);
     });
   });
 });
